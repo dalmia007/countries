a4239e8291daf8297e78f8cf3994631ccc334732
Anurag Dalmia <52538845+dalmia007@users.noreply.github.com>
2022-10-07 04:09:38 +0530

    fix: removed v-eager

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,10 +23,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    { src: '~/plugins/vue-unicons', mode: 'client' },
-    { src: '~/plugins/eager', mode: 'client' },
-  ],
+  plugins: [{ src: '~/plugins/vue-unicons', mode: 'client' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
--- plugins/eager.js
+++
@@ -1,4 +0,0 @@
-import VEager from '@kuroski/v-eager'
-import Vue from 'vue'
-
-Vue.use(VEager)
