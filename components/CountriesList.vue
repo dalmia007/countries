3aa529ebd3d1bac29cb7e8233d0ca0c82741256f
Anurag Dalmia <52538845+dalmia007@users.noreply.github.com>
2022-10-07 00:28:40 +0530

    feat: added search functionality

--- components/CountriesList.vue
+++ components/CountriesList.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="bg-[#FAF9F6]">
-    <div
-      v-for="country in list"
-      :key="country.name"
-      class="bg-white shadow m-2"
-    >
-      <n-link :to="`/country/${country.name}`">
-        <div class="flex items-center space-x-6 p-6">
-          <div class="scale-[200%]">{{ country.flag }}</div>
-          <h3 class="text-sm font-light text-gray-900">
-            {{ country.name }}
-          </h3>
-        </div>
-      </n-link>
+  <div class="flex flex-col">
+    <div class="sticky top-0 shadow-sm z-10">
+      <input
+        v-model="searchQuery"
+        type="text"
+        class="block w-full bg-black pr-10 sm:text-sm h-20 text-white placeholder-white pl-10"
+        placeholder="Search for a country here..."
+      />
+      <div
+        class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3"
+      >
+        <clientOnly>
+          <unicon name="search" fill="white"></unicon>
+        </clientOnly>
+      </div>
+    </div>
+    <div class="bg-[#FAF9F6]">
+      <div
+        v-for="country in filteredList"
+        :key="country.name"
+        class="bg-white shadow m-2"
+      >
+        <n-link :to="`/country/${country.name}`">
+          <div class="flex items-center space-x-6 p-6">
+            <div class="scale-[200%]">{{ country.flag }}</div>
+            <h3 class="text-sm font-light text-gray-900">
+              {{ country.name }}
+            </h3>
+          </div>
+        </n-link>
+      </div>
     </div>
   </div>
 </template>
@@ -22,6 +39,25 @@ export default {
   name: 'CountriesList',
   props: {
     list: { default: null, type: Array },
+  },
+  data() {
+    return {
+      searchQuery: null,
+    }
+  },
+  computed: {
+    filteredList() {
+      if (this.searchQuery && this.list) {
+        return this.list.filter((item) => {
+          return this.searchQuery
+            .toLowerCase()
+            .split(' ')
+            .every((alpha) => item.name.toLowerCase().includes(alpha))
+        })
+      } else {
+        return this.list
+      }
+    },
   },
 }
 </script>
--- plugins/vue-unicons.js
+++ plugins/vue-unicons.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Unicon from 'vue-unicons/dist/vue-unicons-vue2.umd'
-import { uniTimes, uniArrowLeft } from 'vue-unicons/dist/icons'
+import { uniTimes, uniArrowLeft, uniSearch } from 'vue-unicons/dist/icons'
 
-Unicon.add([uniTimes, uniArrowLeft])
+Unicon.add([uniTimes, uniArrowLeft, uniSearch])
 Vue.use(Unicon)
